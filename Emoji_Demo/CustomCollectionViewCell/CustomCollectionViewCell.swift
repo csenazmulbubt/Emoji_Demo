8dc34c4aa72cf45499b4d902e0b8179a1f64fc4a
Nazmul Hoda <cse.nazmul.bubt@gmail.com>
2023-01-24 23:04:46 +0600

    [Edit] add collectionView

---
+++ Emoji_Demo/CustomCollectionViewCell/CustomCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCollectionViewCell.swift
+//  Emoji_Demo
+//
+//  Created by Nazmul on 23/01/2023.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var commonLabel: UILabel!
+    
+    
+}
